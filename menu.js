dfd9733dedc12a74aa66968ecb6f2e89feb93208
Kwshro <112804380+Kwshro@users.noreply.github.com>
2023-05-19 18:06:51 +0200

    menu fix

--- menu.js
+++ menu.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menuContainer");
+const hideMenu = document.querySelector("main");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  hideMenu.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
+  hideMenu.classList.remove("active");
 }))
